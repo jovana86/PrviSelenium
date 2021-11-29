4e75fda6a71046a4e200b4f164a23cc0a67f0bb1
jovana86 <basaric.jovana@yahoo.com>
2021-11-29 20:53:40 +0100

    /

--- src/test/java/MainMenuTest.java
+++ src/test/java/MainMenuTest.java
@@ -3,6 +3,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -15,7 +18,7 @@ public class MainMenuTest {
 
         WebDriver driver = new ChromeDriver ();
 
-        driver.manage ().timeouts ().implicitlyWait ( 10, TimeUnit.SECONDS );
+
         driver.get ( "https://www.saucedemo.com/" );
 
         LoginTest.stampaj ("[TEST] Login to saucedemo.com as a standard user" );
@@ -37,22 +40,27 @@ public class MainMenuTest {
 
 
         WebElement burgerMenuButton = driver.findElement (By.className ( "bm-burger-button" ));
-        Actions action = new Actions ( driver );
-        action.moveToElement ( burgerMenuButton ).perform ();
-        action.click ().build ().perform ();
+        burgerMenuButton.click ();
 
         LoginTest.stampaj ("[TEST] Clicking on about button" );
 
-//        WebElement aboutButton = driver.findElement (By.xpath ( "//*[@id=\"about_sidebar_link\"]" ));
+
+        WebDriverWait wait = new WebDriverWait ( driver,20 );
+        wait.until( ExpectedConditions.presenceOfElementLocated ( By.id("about_sidebar_link")));
+
+
+
+                WebElement aboutButton = driver.findElement (By.id ( "about_sidebar_link" ));
+        aboutButton.click ();
 //
 //        action.moveToElement ( aboutButton ).perform ();
 //        action.click ().build ().perform ();
 
 //        LoginTest.stampaj ("[TEST] Checking if it is on the expected webpage" );
 //
-//        String currentURL = driver.getCurrentUrl ();
-//
-//        assert currentURL.equals ( "https://saucelabs.com/" ): "Error. Wrong URL. Expected to be on sauce labs, but I am on " + currentURL;
+        String currentURL = driver.getCurrentUrl ();
+
+        assert currentURL.equals ( "https://saucelabs.com/" ): "Error. Wrong URL. Expected to be on sauce labs, but I am on " + currentURL;
 
 
 
